fae8266d709da2698f46996908f71428e3a3a769
Mohamed072005 <amine072005@gmail.com>
2024-12-09 20:52:52 +0100

    BOOK-3 implement aws cognito

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AuthWrapper } from "./components/AuthWrapper"
+import { useAuthHandler } from "./hooks/useAuthHandler";
 
-function App() {
-  const [count, setCount] = useState(0)
+const AuthenticatedContent: React.FC = () => {
+  const { auth } = useAuthHandler();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Welcome to the Authenticated Area</h1>
+      <p>Hello, <strong>{auth.user?.profile.email}</strong>!</p>
+      <p>Your ID Token: {auth.user?.id_token}</p>
+      <p>Your Access Token: {auth.user?.access_token}</p>
+      <p>Your Refresh Token: {auth.user?.refresh_token}</p>
+      <button onClick={() => auth.removeUser()}>Sign Out</button>
+    </div>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <AuthWrapper>
+      <AuthenticatedContent />
+    </AuthWrapper>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from 'react-oidc-context';
+
+const cognitoAuthConfig = {
+  authority: import.meta.env.VITE_AUTHORITY,
+  client_id: import.meta.env.VITE_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_REDIRECT_URI,
+  response_type: "code",
+  scope: "email openid phone",
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider { ...cognitoAuthConfig }>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
